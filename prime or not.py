cdd1f9f0a5f98fb90d96ce2de9dfe9cca04730f8
SakshamXI <120104608+SakshamXI@users.noreply.github.com>
2022-12-29 17:32:41 +0530

    Add files via upload
    It tells whether a no. is prime or not

---
+++ prime or not.py
@@ -0,0 +1,11 @@
+n = int(input(" "))
+if n<2:
+    print(" not a prime")
+else:
+   for i in range(2,n):
+       if n%i==0:
+           print("not a prime")
+           break
+   else:
+    print("prime")
+    
