38fdb955cd789bbe78eb2bae8cddbdb67065c6ad
SakshamXI <120104608+SakshamXI@users.noreply.github.com>
2023-04-04 23:45:22 +0530

    Add files via upload
    In this , user can search and it will move him towards the webpage

---
+++ Search.py
@@ -0,0 +1,31 @@
+
+from pywhatkit import *
+def say (a):
+    import pyttsx3
+    pyobj = pyttsx3.init()
+    pyobj . say(a)
+    pyobj . runAndWait()
+
+import time
+t = time.strftime("%H:%M")
+x = time.strftime("%H")
+print("Time->",t)
+if x< "12":
+    print(f"Good Morning Sir,")
+    say(f"Good Morning Sir,")
+elif x<"18":
+    print(f"Good Afternoon Sir,")
+    say(f"Good Afternoon Sir,")
+elif x<"24":
+    print(f"Good Evening Sir,")
+    say(f"Good Evening Sir,")
+print('''What You wanna search,Sir
+Please type it''')
+say('''What You wanna search,Sir
+Please type it
+type your question here
+''') 
+a = input("Type your question here ->")
+
+search(a)
+
