ee9ffd030aec8fb6bac8005804942d112e90c110
SAKSHAM12 <120104608+SakshamXI@users.noreply.github.com>
2023-04-06 14:09:41 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -1,5 +1,5 @@
 import datetime
-x = datetime.datetime.now()
+g = datetime.datetime.now()
 from pywhatkit import *
 def say (a):
     import pyttsx3
@@ -11,7 +11,7 @@ import time
 t = time.strftime("%H:%M")
 x = time.strftime("%H")
 print("Time->",t)
-print("Date->",x.strftime("%b.%d.%Y"))
+print("Date->",g.strftime("%b.%d.%Y"))
 if x< "12":
     print(f"Good Morning Sir,")
     say(f"Good Morning Sir,")
