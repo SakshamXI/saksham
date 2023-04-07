1101d4b46cc91366e8a84dc9a716559257bb8712
SAKSHAM12 <120104608+SakshamXI@users.noreply.github.com>
2023-04-07 21:02:59 +0530

    Add files via upload
    It Takes Password From User And If It Is True It Allows The User Otherwise Takes Value 4 Times And If It Is Not True For The 4th Time , It Will Tell The User To Wait For 30 Seconds

---
+++ Security.py
@@ -0,0 +1,32 @@
+import time
+from pyttsx3 import *
+def bol(a):
+    b = init()
+    b.say(a)
+    b.runAndWait()
+    b.stop()
+    
+g = 1
+a = 9876
+i = 1
+while i < 2:
+    bol("Enter Password")
+    c = int(input("Enter Password"))
+    if c == 9876:
+            print("Allowed")
+            bol("Allowed")
+            break
+    else:
+        if g %4 == 0:
+            print("Try Again After 30 seconds")
+            bol("Try Again After 30 seconds")
+            time.sleep(30)
+            print("Try Now")
+            bol("Try Now")
+        else:
+            print("Not Allowed")
+            bol("Not Allowed")
+    g += 1
+    
+    
+    
