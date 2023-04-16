687407226d257b38131e62412c535482172accb3
SAKSHAM12 <120104608+SakshamXI@users.noreply.github.com>
2023-04-16 15:41:35 +0530

    Add files via upload
    just a reminder

---
+++ reminder.py
@@ -0,0 +1,7 @@
+import time
+from playsound import playsound
+def remind(a):
+    time.sleep(a)
+    playsound("C:\\Users\\saksh\\OneDrive\\Desktop\\__pycache__\\Ring.mp3")
+    playsound("C:\\Users\\saksh\\OneDrive\\Desktop\\__pycache__\\Ring.mp3")
+    
